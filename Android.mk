de0d3d2e106a35504ab2d904d262d72cf28385ff
Marco Zhang <quic_zhangx@quicinc.com>
2023-08-08 16:28:51 +0800

    avoid 32bit build
    Mot-CRs-Fixed: (CR)
    
    Change-Id: If5f685464b8abfbbd87a143fd33f3975382e353e

--- Android.mk
+++ Android.mk
@@ -33,6 +33,7 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES           := $(wildcard $(LOCAL_PATH)/**/*) $(wildcard $(LOCAL_PATH)/*)
 LOCAL_MODULE              := msm_video.ko
 LOCAL_MODULE_KBUILD_NAME  := msm_video.ko
+LOCAL_MULTILIB := first
 LOCAL_MODULE_TAGS         := optional
 LOCAL_MODULE_DEBUG_ENABLE := true
 LOCAL_MODULE_PATH         := $(KERNEL_MODULES_OUT)
